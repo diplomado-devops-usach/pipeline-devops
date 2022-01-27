df9811dcacd338b7d8e4356827c19858e91176a0
Jorge Arias <jorarias.21@gmail.com>
2022-01-26 22:09:26 -0300

    desarrollo del archivo ejecución.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -9,9 +9,41 @@
 
 def call(){
   
-  stage(){
-    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
-  }
+ pipeline {
+    agent any
+	
+	environment {
+	    STAGE = ''
+	}
+	
+	parameters {
+		choice choices: ['gradle', 'maven'], description: 'Indique la herramienta de construcción', name: 'buildTool'
+	}
+
+    stages {
+        stage('Pipeline') {
+            steps {
+                println "Pipeline"
+				script{				
+					if (params.buildTool == 'gradle'){
+						gradle()
+					}
+					else{
+						maven()
+					}
+				}
+            }
+        }
+    }
+	post {
+        failure {
+            slackSend color: '#FF0000', message: "${env.USER} | ${env.JOB_NAME}-${env.BUILD_NUMBER} | ${env.buildTool} | Ejecución fallida en el stage: ${STAGE}\r\n Para ver la salida de la consola haga cilc en ${env.BUILD_URL}console"
+        }
+		success{
+			slackSend color: '#00FF00', message: "${env.USER} | ${env.JOB_NAME} | ${env.buildTool} | Ejecución exitosa. Para obtener más detalles vaya a ${env.BUILD_URL}"
+		}
+    }
+}
 
 }
 
