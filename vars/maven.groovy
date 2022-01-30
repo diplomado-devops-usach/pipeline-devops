3cd6e0f22a3ec776f5b9b329c9731b6e0ff56620
Jorge Arias <jorarias.21@gmail.com>
2022-01-30 01:15:07 -0300

    Separación de stages en métodos para ejecutarlos de forma individual

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -109,7 +109,7 @@ def stageNexus(){
 	stage('uploadNexus') {
 		STAGE = env.STAGE_NAME
 		println "Stage: ${env.STAGE_NAME}"
-		nexusArtifactUploader artifacts: [[artifactId: 'DevOpsUsach2020', classifier: '', file: '/diplomado/modulo3/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar', type: 'jar']], credentialsId: 'nexus-taller10', groupId: 'com.devopsusach2020', nexusUrl: 'f5dd-200-126-115-114.ngrok.io', nexusVersion: 'nexus3', protocol: 'http', repository: 'test-nexus', version: '0.0.1'
+		nexusArtifactUploader artifacts: [[artifactId: 'DevOpsUsach2020', classifier: '', file: '${env.WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar', type: 'jar']], credentialsId: 'nexus-taller10', groupId: 'com.devopsusach2020', nexusUrl: 'localhost:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'test-nexus', version: '0.0.1'
 	}
 }
 
